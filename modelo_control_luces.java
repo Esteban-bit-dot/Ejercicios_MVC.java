b947e045ac8dbc99258c8e07697e895dd4cff995
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 14:15:52 -0500

    Create modelo_control_luces.java

---
+++ modelo_control_luces.java
@@ -0,0 +1,11 @@
+public class LightModel {
+    private boolean isLightOn;
+
+    public boolean isLightOn() {
+        return isLightOn;
+    }
+
+    public void setLightState(boolean state) {
+        this.isLightOn = state;
+    }
+}
