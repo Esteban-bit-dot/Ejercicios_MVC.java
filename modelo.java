05767edecf5241f418bebc6a56ae00aea559ef87
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 08:54:25 -0500

    Update modelo.java

--- modelo.java
+++ modelo.java
@@ -1,1 +1,25 @@
-# Ejercicios_MVC.java
+public class modelo {
+    private String Temperatura;
+    private String Humedad ;
+ 
+    public modelo(){
+       //crear los atributos
+       this.Temperatura="";
+       this.Humedad="";
+       
+    }
+   public String getTemperatura() {
+        return Temperatura;
+    }
+
+    public void setTemperatura(String Temperatura) {
+        this.Temperatura = Temperatura;
+    }
+
+    public String getHumedad() {
+        return Humedad;
+    }
+
+    public void setCedulaCliente(String Humedad) {
+        this.Humedad = Humedad;
+
