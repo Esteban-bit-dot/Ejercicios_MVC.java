877f1c66a5edcd53428871d2c46d801cf87b96e4
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 14:02:32 -0500

    Create Modelo_juego_fizzbuzz.java

---
+++ Modelo_juego_fizzbuzz.java
@@ -0,0 +1,14 @@
+public class Juego_FizzBuzz {
+    public class FizzBuzz {
+    public String calcular(int numero) {
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "FizzBuzz";
+        } else if (numero % 3 == 0) {
+            return "Fizz";
+        } else if (numero % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(numero);
+        }
+    }
+}
