d427eefe1d884be679320f4b11bf44635bff567a
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 13:30:21 -0500

    Create modelo_Sensor_movimiento.java

---
+++ modelo_Sensor_movimiento.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Sensor_Movimiento{
+
+    public static void main(String[] args) {
+        Scanner Tomardatos = new Scanner(System.in);
+        Sensor objSensor  = new Sensor();
+        Tomardatos.close();
+        
+    public String Menu(){ 
+        Menu objMenu = new Menu();
+        objSensor.movimiento="Alerta";
+        sensor();
+        boolean = Alerta;
+       while (true){
+        if (movimiento=Alerta) {
+            System.out.println("Activa");
+            break;
+        }else if(false){
+            System.out.println("desactiva");
+        }
+       }
+        return null;
+    }
