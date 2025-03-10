222d07df43d0830c45ac734edcc1d8ce15272b31
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 13:52:34 -0500

    Update modelo_Sensor_movimiento.java

--- modelo_Sensor_movimiento.java
+++ modelo_Sensor_movimiento.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 public class Sensor_Movimiento{
 
     public static void main(String[] args) {
-        Scanner Tomardatos = new Scanner(System.in);
+        Scanner Sensor = new Scanner(System.in);
         Sensor objSensor  = new Sensor();
-        Tomardatos.close();
-        
-    public String Menu(){ 
-        Menu objMenu = new Menu();
-        objSensor.movimiento="Alerta";
+        Sensor.close();
+ public class Menu_MotionSensor {
+        Menu objMenu = new Menu.nextInt();
+        objSensor movimiento = new movimiento.nextLine() ;
         sensor();
-        boolean = Alerta;
-       while (true){
-        if (movimiento=Alerta) {
-            System.out.println("Activa");
-            break;
-        }else if(false){
-            System.out.println("desactiva");
-        }
-       }
-        return null;
+    private boolean motionDetected;
+
+    public boolean isMotionDetected() {
+        return motionDetected;
+    }
+
+    public void setMotionDetected(boolean motionDetected) {
+        this.motionDetected = motionDetected;
+    }
+}
     }
