ff00b8cf7373e1542a16f715b13d21669e5ba000
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 09:06:52 -0500

    Update controlador.java

--- controlador.java
+++ controlador.java
@@ -1,1 +1,9 @@
-
+public class Controlador {
+    //crear atributos
+    private Temperatura objTeperatura;
+    private Vista objVista;
+    
+    public Controlador(){
+      this.objTemperatura = new Teperatura();
+      this.objVista = new Vista();    
+    }
