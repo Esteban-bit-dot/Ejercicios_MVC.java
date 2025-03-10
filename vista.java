f90621789308dfdd54af2fa6f4fec4675ab40f06
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 08:58:58 -0500

    Update vista.java

--- vista.java
+++ vista.java
@@ -1,1 +1,36 @@
-
+ public  static void Temperatura (){
+        Scanner Temperatura = new Scanner(System.in);
+        Objtemp.temperatura ="Aire";
+        int temp;
+        System.out.print("ingrese la temperatura c°: ");
+        if (temp>28°) {
+            System.out.println("Activado");
+        }else{
+            System.out.println("desactivado");
+        }
+        
+    }
+    
+   public static void Humedad(){
+       int Humedad;
+        objtemp.humedad="Aire";
+        System.out.print("ingrese la humedad: ");
+       if (humedad>60%) {
+           System.out.println("Activado");
+       }else{
+           System.out.print("desactivado");
+       }
+   }
+   public static void Sensor(){
+       boolean = true;
+       while (tem>30°=humdad){
+       if(true){
+       System.out.print("luz verde encendido");
+       }else(false){
+       System.out.print("luz roja apagado");
+       }
+      break;
+   }
+         
+   } 
+}
