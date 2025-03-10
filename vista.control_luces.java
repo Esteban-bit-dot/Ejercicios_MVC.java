96101565fcb5c722d4214672cd2305ca19a32c96
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-10 14:22:11 -0500

    Create vista.control_luces.java

---
+++ vista.control_luces.java
@@ -0,0 +1,26 @@
+    public static void prenderluces(){
+        Scanner luceScanner = new Scanner(System.in);
+        noche objnoche = new noche();
+        objCasa Sensor = new luces.nextInt();
+        objprenderluces.setnocheControl_luces(datonoche);
+        if (prenderluces=noche){
+            boolean = true;
+           System.out.println("luces prendidas");
+        }else(false){
+          System.out.println("luces apagadas");
+        }
+    }
+    public static void Apagarluces(){
+       dia objdia =  new dia();
+       objApagarluces.setdiaControl_luces(datodia);
+       objCasa Sensor = new luces.nextInt();
+      while(luces){
+        if (dia=luces){
+            System.out.println("luces apagadas");
+            boolean = true;
+            break;
+        }else(false){
+            System.out.println("luces prendidas");
+        }
+      }
+   }
