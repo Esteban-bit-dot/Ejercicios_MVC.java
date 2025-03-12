ef5293a87d58fe35a7ba7f9ea09c4cc52f67ebf6
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-12 08:01:15 -0500

    Update Vista_Control_Temperatura.java

--- Vista_Control_Temperatura.java
+++ Vista_Control_Temperatura.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+public class vista {
+ controlador datocontrolador = new controlador();
+ Scanner scanner = new Scanner(System.in);
+
+   System.out.println("Bienvenido al sistema de control de temperatura.");
+    boolean ejecutando = true;
+   while (ejecutando) {
+            System.out.println("\nOpciones:");
+            System.out.println("1. Mostrar temperatura");
+            System.out.println("2.  registrar temperatura(c°)");
+            System.out.println("3. Salir");
+            System.out.print("Elige una opción: ");
+            int opcion = scanner.nextInt();
+      switch (opcion) {
+                case 1:
+                    Controller.mostrarTemperatura();
+                    break;
+                case 2:
+                    System.out.print("Introduce la temperatura(c°): ");
+                    String temperatura= scanner.next();
+                    temperatura datotemperatura = new temperatura();
+
+                    break;
+                case 3:
+                    ejecutando = false;
+                    System.out.println("Gracias por usar el sistema de control. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        }
+        scanner.close();
+}
+
+
+} 
