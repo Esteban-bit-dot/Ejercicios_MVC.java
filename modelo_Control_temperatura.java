91c263abcd27621b9dcbbace8e81c0eb27c8bb46
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-12 07:09:32 -0500

    Update modelo_Control_temperatura.java

--- modelo_Control_temperatura.java
+++ modelo_Control_temperatura.java
@@ -1,2 +1,21 @@
-import java.util.ArrayList;
-
+public  class  modelo {
+  private  String  temperatura;
+  private String Sensor;
+  
+  public void Control(String datoTemperatura, String datoSensor){
+    this.temperatura = datoTemperatura;
+    this.Sensor = datoSensor;
+  }
+  public void getTemperatura(){
+   return Temperatura; 
+  }
+  public void setTemperatura(String datoTemperatura){
+   this.temperatura = new Temperatura();
+  }
+  public void getSensor(){
+   return Sensor;
+  }
+  public void setSensor(String datoSensor){
+  this.Sensor = new Sensor();
+  }
+}
