7029d35b677b5da55dc5aa07d832a7762d6e2502
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-12 07:32:05 -0500

    Update Controlador_Control_temperatura.java

--- Controlador_Control_temperatura.java
+++ Controlador_Control_temperatura.java
@@ -1,1 +1,12 @@
+public class Controlador {
+    private modelo model;
+    private vista view;
 
+  public  controlador(modelo model, vista, view){
+   this modelo = model
+   this vista = view 
+  }
+  public void ejecutartemperatura(number){
+      String resutado = model.getTemperatura(number);
+    view.displayTemperatura(resultado);
+}
