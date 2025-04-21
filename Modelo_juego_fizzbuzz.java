b578e78f3a74fc437ca808c94607c2d0302761b9
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-21 08:11:13 -0500

    Update Modelo_juego_fizzbuzz.java

--- Modelo_juego_fizzbuzz.java
+++ Modelo_juego_fizzbuzz.java
@@ -1,5 +1,8 @@
+import java.util.Arraylist;
 public class Juego_FizzBuzz {
+    private List<Numeros> numeros;
     public class FizzBuzz {
+    
     public String calcular(int numero) {
         if (numero % 3 == 0 && numero % 5 == 0) {
             return "FizzBuzz";
