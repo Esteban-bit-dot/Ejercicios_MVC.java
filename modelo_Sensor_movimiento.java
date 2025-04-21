7d9f0ea7eff13970da0b594f0abac841502155b0
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-21 08:30:31 -0500

    Update modelo_Sensor_movimiento.java

--- modelo_Sensor_movimiento.java
+++ modelo_Sensor_movimiento.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
+import java.util.Arraylist;
 
 public class Modelo_Sensor_Movimiento{
-
+     private List<movimientos> movimientos;
+    
     public static void main(String[] args) {
         Scanner Sensor = new Scanner(System.in);
         Sensor objSensor  = new Sensor();
+        movimientos = new ArrayList<>();
         Sensor.close();
+        
  public class Menu_MotionSensor {
         Menu objMenu = new Menu.nextInt();
         objSensor movimiento = new movimiento.nextLine() ;
         sensor();
+     
     private boolean motionDetected;
 
     public boolean isMotionDetected() {
